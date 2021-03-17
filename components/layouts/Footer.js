bc3baf11d4a601f3280a9fcbd0488ae7c623792a
ide Shouhi <shouhi0922@icloud.com>
2021-03-17 13:16:58 +0900

    fix-color

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 //css(未完成)
 function Footer() {
   return (
-    <footer class="bg-pink">
+    <footer class="bg-red">
       <img src="/TiroKatsu_logo.svg" class="ml-20 w-20 h-15" />
       <p>チロルチョコファンのためのポータルサイト</p>
       <p class="text-center">© 2021 TDD(TIROL Driven Development)</p>
