5a8994d23e66bd8bbfe05dddfb695169581ceb8b
Hoshi Ran <ureshinostyle350@gmail.com>
2021-03-18 11:20:26 +0900

    MyPage の親コンポーネントを MainLayout に

---
+++ pages/mypage.js
@@ -0,0 +1,11 @@
+import MainLayout from "../layouts/Main/index";
+
+const MyPage = function () {
+  return (
+    <MainLayout>
+      <h1>testtest</h1>
+    </MainLayout>
+  );
+};
+
+export default MyPage;
