9ddbfd4081dcde02b4995c67f6c659938c377484
Hoshi Ran <ureshinostyle350@gmail.com>
2021-03-18 09:19:15 +0900

    tailwind.config.js の purge するファイルを変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     //色の保管庫(miroにあったカラーパレットを参照しました)
