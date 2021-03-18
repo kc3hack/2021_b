94c4ec94b415bd09e2d77d71547662fd3443ba2a
Hoshi Ran <ureshinostyle350@gmail.com>
2021-03-18 17:32:47 +0900

    ヘッダーにマイページへのボタンを追加
    Co-authored-by: kyochan <kyochn@users.noreply.github.com>
    Co-authored-by: ikubaku <hide4d51@gmail.com>
    Co-authored-by: shouhi <shouhi@users.noreply.github.com>

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -6,13 +6,18 @@ function Header() {
   return (
     <header>
       <div class="flex flex-row bg-red">
-        <img src="/TiroKatsu_logo.svg" class="ml-20 w-20 h-15" />
+        <Link href={"/"} as={"/"}>
+          <img src="/TiroKatsu_logo.svg" class="ml-20 w-20 h-15" />
+        </Link>
         <p class="ml-5 py-4 text-xs">
           チロルチョコファンのためのポータルサイト
         </p>
         <p class="ml-20 py-4 text-xs">チロルレシピ</p>
         <Link href={"/login"} as={"/login"}>
           <button class="ml-20 py-4 text-xs">ログイン</button>
+        </Link>
+        <Link href={"/mypage"} as={"/mypage"}>
+          <button class="ml-20 py-4 text-xs">マイページ</button>
         </Link>
       </div>
     </header>
