c78e55819a99165ac7cbd32a546bf40d04e79a85
Hoshi Ran <ureshinostyle350@gmail.com>
2021-03-20 02:02:25 +0900

    過去のレビューの表示を実装

--- pages/mypage.js
+++ pages/mypage.js
@@ -1,35 +1,54 @@
 import MainLayout from "../layouts/Main/index";
+import Reviewer from "../components/review";
 
 import firebase from "firebase/app";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useDocumentData } from "react-firebase-hooks/firestore";
+import {
+  useDocumentData,
+  useCollectionData,
+} from "react-firebase-hooks/firestore";
 import { useDownloadURL } from "react-firebase-hooks/storage";
 
 const MyPage = (props) => {
   const [authUser, authLoading, authError] = useAuthState(firebase.auth());
-
   const uid = authUser?.uid;
 
   const [user] = useDocumentData(
     uid && firebase.firestore().doc(`user/${uid}`)
   );
-  console.log(user);
 
-  const [value, loading, error] = useDownloadURL(
-    firebase.storage().ref("user_icon/チロリストちあ吉アイコン72正方形 1.png")
+  const [image_url, loading, error] = useDownloadURL(
+    uid && firebase.storage().ref(`user_icon/${uid}.png`)
+  );
+
+  const [name, setName] = useState("");
+  const postNewName = async () => {
+    await (uid && firebase.firestore().doc(`user/${uid}`)).update({
+      display_name: name,
+    });
+  };
+
+  const [profile, setProfile] = useState("");
+  const postNewProfile = async () => {
+    await (uid && firebase.firestore().doc(`user/${uid}`)).update({
+      profile: profile,
+    });
+  };
+
+  // このユーザの全てのレビューを取得
+  const [reviews] = useCollectionData(
+    uid &&
+      firebase
+        .firestore()
+        .collection("review")
+        .where("user_id_token", "==", uid),
+    { idField: "id" }
   );
 
   return (
     <MainLayout>
-      <h1>{user?.display_name}</h1>
-      {value && <img src={value}></img>}
-    </MainLayout>
-  );
-};
-
-export default MyPage;
       <div className="flex ">
         {image_url && <img src={image_url} className="w-32 h-32"></img>}
         <div className="mx-8">
@@ -56,3 +75,17 @@ export default MyPage;
         </div>
       </div>
 
+      <div className="p-12">
+        <h1>レビュー</h1>
+        <ul>
+          {reviews?.map((review) => {
+            // FIXME: review idをkeyにしたいけど取得方法がわからんねん
+            return <Reviewer key={review?.title} review={review} />;
+          })}
+        </ul>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default MyPage;
