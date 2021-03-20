f47fb3e9b6a03b5790597388028e75adcc1e6218
ikubaku <hide4d51@gmail.com>
2021-03-20 21:34:07 +0900

    Tweak styles

--- components/UserProfileButton.js
+++ components/UserProfileButton.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 const UserProfileButton = (props) => {
   return props.isLoggedIn ? (
     <Link href={"/mypage"} as={"mypage"}>
-      <div className="flex">
-        <img src={props.avatarImageUrl} className="w-32 h-32"></img>
-        <p className="ml-20 py-4 text-xs">{props.userDisplayName}</p>
+      <div className="flex flex-row cursor-pointer">
+        <img src={props.avatarImageUrl} className="place-self-center ml-20 w-8 h-8"></img>
+        <p className="ml-2 py-4 text-xs">{props.userDisplayName}</p>
       </div>
     </Link>
   ) : (
     <Link href={"/login"} as={"/login"}>
-      <button className="ml-20 py-4 text-xs">ログイン</button>
+      <button className="ml-20 py-4 text-xs cursor-pointer">ログイン</button>
     </Link>
   );
 };
