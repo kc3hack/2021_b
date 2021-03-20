5060d9422295f2719a2ae7f927e071b8d0c411e3
ikubaku <hide4d51@gmail.com>
2021-03-20 21:37:24 +0900

    Run prettier

--- components/UserProfileButton.js
+++ components/UserProfileButton.js
@@ -4,7 +4,10 @@ const UserProfileButton = (props) => {
   return props.isLoggedIn ? (
     <Link href={"/mypage"} as={"mypage"}>
       <div className="flex flex-row cursor-pointer">
-        <img src={props.avatarImageUrl} className="place-self-center ml-20 w-8 h-8"></img>
+        <img
+          src={props.avatarImageUrl}
+          className="place-self-center ml-20 w-8 h-8"
+        ></img>
         <p className="ml-2 py-4 text-xs">{props.userDisplayName}</p>
       </div>
     </Link>
