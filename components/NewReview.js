d382ef296b6602d03f500fa96f5b8c7073c943db
Hoshi Ran <ureshinostyle350@gmail.com>
2021-03-20 21:41:14 +0900

    チロレポ投稿機能を実装

---
+++ components/NewReview.js
@@ -0,0 +1,114 @@
+import { useState } from "react";
+
+import firebase from "firebase/app";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  useDocumentData,
+  useCollectionData,
+} from "react-firebase-hooks/firestore";
+import { useDownloadURL } from "react-firebase-hooks/storage";
+
+const NewReview = (props) => {
+  // 認証情報取得
+  const [authUser, authLoading, authError] = useAuthState(firebase.auth());
+  const uid = authUser?.uid;
+
+  // ユーザデータ取得
+  const [user] = useDocumentData(
+    uid && firebase.firestore().doc(`user/${uid}`)
+  );
+
+  // ユーザアイコン取得
+  const [image_path, loading, error] = useDownloadURL(
+    uid && firebase.storage().ref(`user_icon/${uid}.png`)
+  );
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [score, setScore] = useState();
+
+  const [isWritten, setIsWritten] = useState(false);
+
+  // 投稿処理
+  const postNewReview = async () => {
+    await (uid && firebase.firestore().collection("review")).add({
+      content: content,
+      date: firebase.firestore.Timestamp.now(), //fromDate(Date.now()),
+      score: score,
+      tirol_id: props.tirol_id,
+      title: title,
+      user_id_token: uid,
+    });
+    setTitle("");
+    setContent("");
+    document.getElementById("default-score").selected = true;
+  };
+
+  if (!uid) {
+    return <></>;
+  }
+
+  return (
+    <div className="flex rounded-sm">
+      <div>
+        {image_path && <img src={image_path} className="w-12"></img>}
+        <h1>{user?.display_name}</h1>
+      </div>
+
+      <div className="px-4 w-full bg-white">
+        <div className="">
+          <input
+            type="text"
+            placeholder="タイトルを入力してください"
+            minLength={4}
+            className="py-2 outline-none w-full"
+            value={title}
+            required
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <select
+            name="score"
+            className="p-1 bg-pink"
+            onChange={(e) => setScore(e.target.value)}
+            required
+          >
+            <option value="" id="default-score">
+              5段階評価するならいくつ星？
+            </option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+        </div>
+
+        <div className="">
+          <input
+            type="text"
+            placeholder="このチロルチョコへの思い・感想を書こう！"
+            minLength={4}
+            className=" py-2 outline-none w-full pb-8"
+            value={content}
+            required
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+
+        <button
+          type="button"
+          // disabled={!isWritten}
+          className="m-1  px-4 py-2 float-right bg-pink rounded-sm"
+          onClick={postNewReview}
+        >
+          投稿
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NewReview;
--- pages/tirolrepo.js
+++ pages/tirolrepo.js
@@ -1,45 +1,48 @@
 import MainLayout from "../layouts/Main/index";
 import Reviewer from "../components/reviewer";
+import NewReview from "../components/NewReview";
+
+import { useRouter } from "next/router";
 
 import firebase from "firebase/app";
 import {
   useDocumentData,
   useCollectionData,
 } from "react-firebase-hooks/firestore";
-import { useDownloadURL } from "react-firebase-hooks/storage";
-import { useRouter } from "next/router";
-import { route } from "next/dist/next-server/server/router";
 
 const TirolRepo = () => {
   const router = useRouter();
-  const tirol_id = router.query.name;
-  console.log(tirol_id);
+  const tirol_id = router.query.name || "";
 
   // チロルデータ取得
   const [tirol_doc] = useDocumentData(
     tirol_id && firebase.firestore().doc(`tirol/${tirol_id}`)
   );
 
-  // チロル画像取得
-  const [image_path] = useDownloadURL(
-    tirol_id && firebase.storage().ref(`tirol/${tirol_id}.png`)
-  );
-
   // このチロルの全てのレビューを取得
   const [reviews] = useCollectionData(
-    firebase.firestore().collection("review").where("tirol_id", "==", tirol_id),
+    firebase
+      .firestore()
+      .collection("review")
+      ?.where("tirol_id", "==", tirol_id),
     { idField: "id" }
   );
 
-  console.log(reviews);
-
   return (
     <MainLayout>
-      <h1>{tirol_doc?.name}</h1>
-      <h1>星評価{tirol_doc?.average_score}</h1>
-      {image_path && <img src={image_path}></img>}
-      <div className="p-12">
-        <h1>レビュー</h1>
+      <div className="flex">
+        {<img src={tirol_doc?.image}></img>}
+        <div>
+          <h1>{tirol_doc?.name}</h1>
+          <h1>星評価{tirol_doc?.average_score}</h1>
+        </div>
+      </div>
+
+      <div className="py-12">
+        <NewReview tirol_id={tirol_id} />
+
+        <h1>みんなのチロレポ</h1>
+
         <ul>
           {reviews?.map((review) => {
             return <Reviewer key={review?.user_id_token} review={review} />;
