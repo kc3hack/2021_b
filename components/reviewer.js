fb8ea116b9fb30c79c4996c9ee43c739cd497460
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-03-20 13:29:58 +0900

    prettierを実行した

--- components/reviewer.js
+++ components/reviewer.js
@@ -2,37 +2,34 @@ import firebase from "firebase/app";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 import { useDownloadURL } from "react-firebase-hooks/storage";
 
-
-import dayjs from 'dayjs'
-
+import dayjs from "dayjs";
 
 const Reviewer = (props) => {
-    // レビュアーデータ取得
-    const uid = props.review?.user_id_token
-    const [user] = useDocumentData(
-        uid && firebase.firestore().doc(`user/${uid}`)
-    );
+  // レビュアーデータ取得
+  const uid = props.review?.user_id_token;
+  const [user] = useDocumentData(
+    uid && firebase.firestore().doc(`user/${uid}`)
+  );
 
-    const [image_path, loading, error] = useDownloadURL(
-        uid && firebase.storage().ref(`user_icon/${uid}.png`)
-    );
+  const [image_path, loading, error] = useDownloadURL(
+    uid && firebase.storage().ref(`user_icon/${uid}.png`)
+  );
 
-    const convertSecondToDate = (timestamp) => {
-        return dayjs(timestamp * 1000).format("YYYY/MM/DD HH:MM")
-    }
+  const convertSecondToDate = (timestamp) => {
+    return dayjs(timestamp * 1000).format("YYYY/MM/DD HH:MM");
+  };
 
-    return (
-        <li className="my-8">
-            <h1>{user?.display_name}</h1>
-            {image_path && <img src={image_path} className="w-12"></img>}
+  return (
+    <li className="my-8">
+      <h1>{user?.display_name}</h1>
+      {image_path && <img src={image_path} className="w-12"></img>}
 
-            <h1>{props.review?.title}</h1>
-            <h1>score: {props.review?.score}</h1>
-            <h1>{props.review?.content}</h1>
-            <div>{convertSecondToDate(props.review?.date.seconds)}投稿</div>
-        </li>
-    )
-}
-
+      <h1>{props.review?.title}</h1>
+      <h1>score: {props.review?.score}</h1>
+      <h1>{props.review?.content}</h1>
+      <div>{convertSecondToDate(props.review?.date.seconds)}投稿</div>
+    </li>
+  );
+};
 
 export default Reviewer;
--- pages/tirolrepo.js
+++ pages/tirolrepo.js
@@ -1,49 +1,49 @@
 import MainLayout from "../layouts/Main/index";
 import Reviewer from "../components/reviewer";
 
-
 import firebase from "firebase/app";
-import { useDocumentData, useCollectionData } from "react-firebase-hooks/firestore";
+import {
+  useDocumentData,
+  useCollectionData,
+} from "react-firebase-hooks/firestore";
 import { useDownloadURL } from "react-firebase-hooks/storage";
 
-
 const TirolRepo = () => {
-    const tirol_id = "RQ1HqfOdsovmWym7jv3L";
+  const tirol_id = "RQ1HqfOdsovmWym7jv3L";
 
-    // チロルデータ取得
-    const [tirol_doc] = useDocumentData(
-        tirol_id && firebase.firestore().doc(`tirol/${tirol_id}`)
-    );
+  // チロルデータ取得
+  const [tirol_doc] = useDocumentData(
+    tirol_id && firebase.firestore().doc(`tirol/${tirol_id}`)
+  );
 
-    // チロル画像取得
-    const [image_path] = useDownloadURL(
-        tirol_id && firebase.storage().ref(`tirol/${tirol_id}.png`)
-    );
+  // チロル画像取得
+  const [image_path] = useDownloadURL(
+    tirol_id && firebase.storage().ref(`tirol/${tirol_id}.png`)
+  );
 
-    // このチロルの全てのレビューを取得
-    const [reviews] = useCollectionData(
-        firebase.firestore().collection("review").where('tirol_id', '==', tirol_id),
-        { idField: 'id' }
-    );
+  // このチロルの全てのレビューを取得
+  const [reviews] = useCollectionData(
+    firebase.firestore().collection("review").where("tirol_id", "==", tirol_id),
+    { idField: "id" }
+  );
 
-    console.log(reviews)
+  console.log(reviews);
 
-    return (
-        <MainLayout>
-            <h1>{tirol_doc?.name}</h1>
-            <h1>星評価{tirol_doc?.average_score}</h1>
-            {image_path && <img src={image_path}></img>}
-            <div className="p-12">
-                <h1>レビュー</h1>
-                <ul>
-                    {reviews?.map(review => {
-                        return <Reviewer key={review?.user_id_token} review={review} />
-                    })}
-                </ul>
-            </div>
-
-        </MainLayout>
-    );
+  return (
+    <MainLayout>
+      <h1>{tirol_doc?.name}</h1>
+      <h1>星評価{tirol_doc?.average_score}</h1>
+      {image_path && <img src={image_path}></img>}
+      <div className="p-12">
+        <h1>レビュー</h1>
+        <ul>
+          {reviews?.map((review) => {
+            return <Reviewer key={review?.user_id_token} review={review} />;
+          })}
+        </ul>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default TirolRepo;
