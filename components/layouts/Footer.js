89967f4abe5f5af099c7abe29fe1c87f4dfd6b10
ikubaku <hide4d51@gmail.com>
2021-03-21 13:43:58 +0900

    Run prettier

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -7,10 +7,14 @@ function Footer() {
       <div className="grid grid-cols-2 justify-items-center">
         <div className="grid grid-rows-2 text-center justify-items-center">
           <img src="/TiroKatsu_logo.svg" className="mt-10 w-36 h-10" />
-          <p className="mb-10 text-white text-xs">チロルチョコファンのためのポータルサイト</p>
+          <p className="mb-10 text-white text-xs">
+            チロルチョコファンのためのポータルサイト
+          </p>
         </div>
         <div className="flex">
-          <p className="self-center text-white text-xs">© 2021 TDD(TIROL Driven Development)</p>
+          <p className="self-center text-white text-xs">
+            © 2021 TDD(TIROL Driven Development)
+          </p>
         </div>
       </div>
     </footer>
