d8cd5ee39b5fa58bc0dde694b5017b1e3e772b9c
ikubaku <hide4d51@gmail.com>
2021-03-21 13:43:32 +0900

    Update the footer layout and style

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -4,9 +4,15 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-red">
-      <img src="/TiroKatsu_logo.svg" className="ml-20 w-20 h-15" />
-      <p>チロルチョコファンのためのポータルサイト</p>
-      <p className="text-center">© 2021 TDD(TIROL Driven Development)</p>
+      <div className="grid grid-cols-2 justify-items-center">
+        <div className="grid grid-rows-2 text-center justify-items-center">
+          <img src="/TiroKatsu_logo.svg" className="mt-10 w-36 h-10" />
+          <p className="mb-10 text-white text-xs">チロルチョコファンのためのポータルサイト</p>
+        </div>
+        <div className="flex">
+          <p className="self-center text-white text-xs">© 2021 TDD(TIROL Driven Development)</p>
+        </div>
+      </div>
     </footer>
   );
 }
